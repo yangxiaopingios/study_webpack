48f9a56e839a4a6407bcebafe31ff669939a8e78
yangxiooping <yangxiaoping@jd.com>
2018-06-08 11:00:08 +0800

    清空文件

--- src/another.js
+++
@@ -1,9 +0,0 @@
-/*
-* @Author: yangxiooping
-* @Date:   2018-06-07 17:34:35
-* @Last Modified by:   yangxiooping
-* @Last Modified time: 2018-06-07 17:35:14
-*/
-import printMe from './print.js';
-
-printMe();
--- src/print.js
+++
@@ -1,9 +0,0 @@
-/*
-* @Author: yangxiooping
-* @Date:   2018-06-07 10:19:55
-* @Last Modified by:   yangxiooping
-* @Last Modified time: 2018-06-07 18:43:12
-*/
-export default function printMe() {
-  console.log('I get called from print.js!');
-}
